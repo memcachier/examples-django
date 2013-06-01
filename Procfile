6f1f3b3c0624374515108179f56f9f94e7aa936c
David Terei <davidterei@gmail.com>
2013-06-01 03:48:38 -0700

    add procfile for launching

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn memcachier_algebra.wsgi
--- memcachier_algebra/urls.py
+++ memcachier_algebra/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     url(r'^$', 'memcachier_algebra.views.home', name='home'),
-    url(r'^compute/', 'memcachier_algebra.views.compute', name='compute'),
+    url(r'^compute/$', 'memcachier_algebra.views.compute', name='compute'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
