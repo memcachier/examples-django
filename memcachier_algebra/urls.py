b559059d4f94dadc00b17a0865d7319db9d0ef2e
David Terei <davidterei@gmail.com>
2013-09-17 14:17:09 -0700

    Tweak routes.

--- memcachier_algebra/urls.py
+++ memcachier_algebra/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
+    url(r'^compute$', 'memcachier_algebra.views.compute', name='compute'),
     url(r'^$', 'memcachier_algebra.views.home', name='home'),
-    url(r'^compute/', 'memcachier_algebra.views.compute', name='compute'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
